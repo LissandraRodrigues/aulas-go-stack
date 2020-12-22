562ddb962fd69c9d6ef8a529da789e4f75ca53a5
Luiza Lissandra <luizalissandrarosa@poli.ufrj.br>
2020-12-22 13:36:55 -0300

    Update avatar. Back-end of Go Barber.  Status: Inconcluded

--- nivel-02/iniciando-back-end/src/routes/users.routes.ts
+++ nivel-02/iniciando-back-end/src/routes/users.routes.ts
@@ -1,9 +1,14 @@
 import { Router } from 'express';
 
+import multer from 'multer';
+import uploadConfig from '../config/upload';
+
 import CreateUserService from '../services/CreateUserService';
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 
 const usersRouter = Router();
+const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (request, response) => {
     try {
@@ -25,8 +30,26 @@ usersRouter.post('/', async (request, response) => {
     }
 });
 
-usersRouter.patch('/avatar', ensureAuthenticated, async (request, response) => {
-    return response.json({ ok: true });
-});
+usersRouter.patch(
+    '/avatar',
+    ensureAuthenticated,
+    upload.single('avatar'),
+    async (request, response) => {
+        try {
+            const updateUserAvatar = new UpdateUserAvatarService();
+
+            const user = await updateUserAvatar.execute({
+                user_id: request.user.id,
+                avatarFilename: request.file.filename,
+            });
+
+            delete user.password;
+
+            return response.json(user);
+        } catch (err) {
+            return response.status(400).json({ error: err.message });
+        }
+    },
+);
 
 export default usersRouter;
