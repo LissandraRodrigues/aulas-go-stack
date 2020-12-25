0684a1946212ee44354fead29bd7c2107d39c647
Luiza Lissandra <luizalissandrarosa@poli.ufrj.br>
2020-12-25 11:54:50 -0300

    Add of routes

--- nivel-03/primeiro-projeto-react/src/routes/index.tsx
+++ nivel-03/primeiro-projeto-react/src/routes/index.tsx
@@ -5,8 +5,9 @@ import Dashboard from "./pages/Dashboard";
 import Repository from "./pages/Repository";
 
 const Routes: React.FC = () => (
-    <Switch>
-        <Route path="/" component={Dashboard} />
+  <Switch>
+    <Route path="/" exact component={Dashboard} />
+    <Route path="/repository" component={Repository} />
   </Switch>
 );
 
