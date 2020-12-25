57a886051d3648e55e0f523ceb127cc6203a9a2f
Luiza Lissandra <luizalissandrarosa@poli.ufrj.br>
2020-12-25 11:41:29 -0300

    Delete of patterns

---
+++ nivel-03/primeiro-projeto-react/src/routes/pages/Dashboard/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+// FC -> Function Component.
+const Dashboard: React.FC = () => {
+    return <h1> Dashboard </h1>;
+};
+
+export default Dashboard;
---
+++ nivel-03/primeiro-projeto-react/src/routes/pages/Repository/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+// FC -> Function Component.
+const Repository: React.FC = () => {
+    return <h1> Repository </h1>;
+};
+
+export default Repository;
