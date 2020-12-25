5ebeb96faae0d3d09aa8cc3996f96d15bd5ed5ad
Luiza Lissandra <luizalissandrarosa@poli.ufrj.br>
2020-12-25 12:34:07 -0300

    Add beginning of style components

--- nivel-03/primeiro-projeto-react/src/routes/pages/Dashboard/index.tsx
+++ nivel-03/primeiro-projeto-react/src/routes/pages/Dashboard/index.tsx
@@ -1,8 +1,14 @@
 import React from "react";
 
+import { Title } from './styles';
+
 // FC -> Function Component.
 const Dashboard: React.FC = () => {
-    return <h1> Dashboard </h1>;
+    return (
+    
+        <Title> Explore repositórios no Github. </Title>
+        
+    );
 };
 
 export default Dashboard;
