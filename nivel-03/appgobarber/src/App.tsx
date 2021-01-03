ccee530fdafc21f5c37fcec654210a8f05a18d6e
Luiza Lissandra <luizalissandrarosa@poli.ufrj.br>
2021-01-03 13:07:18 -0300

    Initial commit with editor config, eslint and prettier

---
+++ nivel-03/appgobarber/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import {Text, View} from 'react-native';
+
+const App: React.FC = () => (
+  <View>
+    <Text> Hello, World</Text>
+  </View>
+);
+
+export default App;
