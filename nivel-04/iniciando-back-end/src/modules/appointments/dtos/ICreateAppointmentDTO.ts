953ee396d331a8e79dbf9b4d6fe02fa491193e46
Luiza Lissandra <luizalissandrarosa@poli.ufrj.br>
2021-01-07 10:23:42 -0300

    Rewriting repositories

---
+++ nivel-04/iniciando-back-end/src/modules/appointments/dtos/ICreateAppointmentDTO.ts
@@ -0,0 +1,8 @@
+// Define qual é o formato dos dados que são necessários para se criar um appoinment.
+
+export default interface ICreateAppointmentDTO {
+
+    provider_id: string;
+    date: Date;
+
+}
--- nivel-04/iniciando-back-end/src/modules/appointments/repositories/IAppointmentsRepository.ts
+++ nivel-04/iniciando-back-end/src/modules/appointments/repositories/IAppointmentsRepository.ts
@@ -1,6 +1,10 @@
 import Appointment from '../infra/typeorm/entities/Appointment';
 
- export default interface IAppointmentsRepository {
+import ICreateAppoinmentDTO from '../dtos/ICreateAppointmentDTO';
+
+export default interface IAppointmentsRepository {
+
+    create(data: ICreateAppoinmentDTO): Promise<Appointment>;
 
     findByDate(date: Date): Promise<Appointment | undefined>
 
