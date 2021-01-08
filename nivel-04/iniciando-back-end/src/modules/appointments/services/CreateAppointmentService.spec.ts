fab2650694f6a42bead8f4f1c2d347fb06953bc1
Luiza Lissandra <luizalissandrarosa@poli.ufrj.br>
2021-01-08 19:18:07 -0300

    Creating the first test

--- nivel-04/iniciando-back-end/src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ nivel-04/iniciando-back-end/src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,5 +1,38 @@
-test('Sum two numbers', () => {
+// import 'reflect-metadata';
 
-    expect(1 + 2).toBe(3);
+import FakeAppointmentRepository from '../repositories/fakes/FakeAppointmentsRepository';
+import CreateAppointmentService from './CreateAppointmentService';
+
+describe('CreateAppointment', () => {
+
+    it('should be able to create a new appointment', async () => {
+
+        const fakeAppointmentRepository = new FakeAppointmentRepository();
+
+        const createAppointment = new CreateAppointmentService(fakeAppointmentRepository);
+
+        const appointment = await createAppointment.execute({
+
+            date: new Date(),
+            provider_id: "1233333333"
+
+        });
+
+        expect(appointment).toHaveProperty('id');
+        expect(appointment.provider_id).toBe('1233333333');
+
+    })
 
 });
+
+
+describe('CreateAppointment', () => {
+
+    it('should not to be able to create two appointments on the same time', () => {
+
+        expect(1 + 2).toBe(3);
+
+    })
+
+});
+
