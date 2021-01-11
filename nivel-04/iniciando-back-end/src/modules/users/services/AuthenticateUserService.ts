710aa76bfb59ec5b9d11ccf7fa53aa281af52283
Luiza Lissandra <luizalissandrarosa@poli.ufrj.br>
2021-01-11 12:39:24 -0300

    User creation and authentication tests

--- nivel-04/iniciando-back-end/src/modules/users/services/AuthenticateUserService.ts
+++ nivel-04/iniciando-back-end/src/modules/users/services/AuthenticateUserService.ts
@@ -1,4 +1,3 @@
-import { compare } from 'bcryptjs';
 import { sign } from 'jsonwebtoken';
 import authConfig from '@config/auth';
 
@@ -9,6 +8,8 @@ import AppError from '@shared/errors/AppError';
 import IUsersRepository from '../repositories/IUsersRepository';
 
 import User from '../infra/typeorm/entities/User';
+
+import IHashProvider from '../providers/HashProvider/models/IHashProvider';
 
 interface IRequest {
     email: string;
@@ -26,6 +27,9 @@ class AuthenticateUserService {
     constructor(
         @inject('UsersRepository')
         private usersRepository: IUsersRepository,
+
+        @inject('HashProvider')
+        private hashProvider: IHashProvider,
     ) {}
 
     public async execute({ email, password }: IRequest): Promise<IResponse> {
@@ -38,7 +42,7 @@ class AuthenticateUserService {
 
         // user.password -> Senha criptografada.
         // password -> Senha não-criptografada (é a tentativa de login).
-        const passwordMatched = await compare(password, user.password);
+        const passwordMatched = await this.hashProvider.compareHash(password, user.password);
 
         if (!passwordMatched) {
             throw new AppError('Incorrect email/password combination!', 401);
--- nivel-04/iniciando-back-end/src/modules/users/services/CreateUserService.ts
+++ nivel-04/iniciando-back-end/src/modules/users/services/CreateUserService.ts
@@ -1,11 +1,11 @@
-import { hash } from 'bcryptjs';
-
 import { injectable, inject } from 'tsyringe';
 
 import AppError from '@shared/errors/AppError';
 import User from '../infra/typeorm/entities/User';
 
 import IUsersRepository from '../repositories/IUsersRepository';
+
+import IHashProvider from '../providers/HashProvider/models/IHashProvider';
 
 interface IRequest {
     name: string;
@@ -20,6 +20,9 @@ class CreateUserService {
         @inject('UsersRepository')
         private usersRepository: IUsersRepository,
 
+        @inject('HashProvider')
+        private hashProvider: IHashProvider,
+
     ) {}
 
     public async execute({ name, email, password }: IRequest): Promise<User> {
@@ -31,7 +34,7 @@ class CreateUserService {
         }
 
         // Criptografia da senha do usuário.
-        const hashedPassword = await hash(password, 8);
+        const hashedPassword = await this.hashProvider.generateHash(password);
 
         const user = await this.usersRepository.create({
             name,
