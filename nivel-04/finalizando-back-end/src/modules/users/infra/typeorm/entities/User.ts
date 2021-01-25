23791d065f5f4ff60199f7ebb78a2d353b3ac49d
Luiza Lissandra <luizalissandrarosa@poli.ufrj.br>
2021-01-25 19:33:39 -0300

    Upload files to Amazon S3

--- nivel-04/finalizando-back-end/src/modules/users/infra/typeorm/entities/User.ts
+++ nivel-04/finalizando-back-end/src/modules/users/infra/typeorm/entities/User.ts
@@ -8,6 +8,8 @@ import {
 
 import { Exclude, Expose } from 'class-transformer';
 
+import uploadConfig from '@config/upload';
+
 @Entity('users')
 class User {
     @PrimaryGeneratedColumn('uuid')
@@ -36,11 +38,29 @@ class User {
     @Expose({ name: 'avatar_url' })
     getAvatarUrl(): string | null {
 
-        return this.avatar
-            ? `${process.env.APP_API_URL}/files/${this.avatar}`
-            : null;
+        if(!this.avatar) {
+
+            return null;
+
+        }
+
+        switch(uploadConfig.driver) {
+
+            case 'disk':
+
+                return `${process.env.APP_API_URL}/files/${this.avatar}`
+
+            case 's3':
+
+                return `https://${uploadConfig.config.aws.bucket}.s3.amazonaws.com/${this.avatar}`
+
+            default:
+
+                return null;
+        }
 
     }
+
 }
 
 export default User;
