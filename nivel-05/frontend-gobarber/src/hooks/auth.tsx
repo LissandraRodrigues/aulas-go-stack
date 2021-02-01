775cafef4d057116b94ca5f66c00c31004d38e37
Luiza Lissandra <luizalissandrarosa@poli.ufrj.br>
2021-02-01 19:56:30 -0300

    Creating header and next schedule

--- nivel-05/frontend-gobarber/src/hooks/auth.tsx
+++ nivel-05/frontend-gobarber/src/hooks/auth.tsx
@@ -2,20 +2,26 @@ import React, { createContext, useCallback, useState, useContext } from 'react';
 
 import api from '../services/api';
 
+interface User {
+  id: string;
+  name: string;
+  avatar_url: string;
+}
+
 interface SignInCredentials {
   email: string;
   password: string;
 }
 
 interface AuthContextData {
-  user: object;
+  user: User;
   signIn(credentials: SignInCredentials): Promise<void>;
   signOut(): void;
 }
 
 interface AuthState {
   token: string;
-  user: object;
+  user: User;
 }
 
 const AuthContext = createContext<AuthContextData>({} as AuthContextData);
--- nivel-05/frontend-gobarber/src/pages/Dashboard/index.tsx
+++ nivel-05/frontend-gobarber/src/pages/Dashboard/index.tsx
@@ -1,5 +1,75 @@
 import React from 'react';
 
-const Dashboard: React.FC = () => <h1> Dashboard </h1>;
+import { FiClock, FiPower } from 'react-icons/fi';
+import {
+  Container,
+  Header,
+  HeaderContent,
+  Profile,
+  Schedule,
+  Content,
+  NextAppointment,
+  Calendar,
+} from './styles';
+
+import logoImg from '../../assets/logo.svg';
+import { useAuth } from '../../hooks/auth';
+
+const Dashboard: React.FC = () => {
+  const { signOut, user } = useAuth();
+
+  return (
+    <Container>
+      <Header>
+        <HeaderContent>
+          <img src={logoImg} alt="GoBarber" />
+
+          <Profile>
+            <img src={user.avatar_url} alt={user.name} />
+            <div>
+              <span> Bem vindo, </span>
+              <strong> {user.name} </strong>
+            </div>
+          </Profile>
+
+          <button type="button" onClick={signOut}>
+            <FiPower />
+          </button>
+        </HeaderContent>
+      </Header>
+
+      <Content>
+        <Schedule>
+          <h1> Horários agendados </h1>
+          <p>
+            <span> Hoje </span>
+            <span> Dia 01 </span>
+            <span> Segunda-feira </span>
+          </p>
+
+          <NextAppointment>
+            <strong> Atendimento a seguir </strong>
+
+            <div>
+              <img
+                src="http://localhost:3333/files/25b6870f04bbfa6642d3-0_720x720.jpg"
+                alt="Luiza Lissandra"
+              />
+
+              <strong> Luiza Lissandra </strong>
+
+              <span>
+                <FiClock />
+                08:00
+              </span>
+            </div>
+          </NextAppointment>
+        </Schedule>
+
+        <Calendar />
+      </Content>
+    </Container>
+  );
+};
 
 export default Dashboard;
