e38fda132a74ba35d7684e95b6f15e86e12ead53
Luiza Lissandra <luizalissandrarosa@poli.ufrj.br>
2021-02-01 11:13:49 -0300

    Modification of reset route

--- nivel-05/backend-gobarber/src/modules/users/services/SendForgotPasswordEmailService.ts
+++ nivel-05/backend-gobarber/src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -59,7 +59,7 @@ class SendForgotPasswordEmailService {
                 variables: {
 
                     name: user.name,
-                    link: `${process.env.APP_WEB_URL}/reset_password?token=${token}`,
+                    link: `${process.env.APP_WEB_URL}/reset-password?token=${token}`,
 
                 }
 
