549b1012ec7d4c21fbc8785eded9efc1723c7abe
Luiza Lissandra <luizalissandrarosa@poli.ufrj.br>
2021-02-05 19:38:10 -0300

    Creating screen structure

--- nivel-05/mobile-gobarber/src/pages/AppointmentCreated/index.tsx
+++ nivel-05/mobile-gobarber/src/pages/AppointmentCreated/index.tsx
@@ -1,11 +1,50 @@
-import React from 'react';
-import { View, Button } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import React, { useCallback } from 'react';
+
+import Icon from 'react-native-vector-icons/Feather';
+
+import { Container, Title, Description, OkButton, OkButtonText } from './styles';
 
 const AppointmentCreated: React.FC = () => {
 
+  const { reset } = useNavigation();
+
+  const handleOkPressed = useCallback(() => {
+
+    reset({
+
+      routes: [
+
+        {
+
+          name: 'Dashboard'
+
+        }
+
+      ],
+
+      index: 0
+
+    });
+
+   }, [reset]);
+
   return (
 
-    <View />
+    <Container>
+
+      <Icon name="check" size={80} color="#04d361"/>
+
+      <Title> Agendamento concluído </Title>
+      <Description> Terça, dia 14 de março de 2021 às 10:00 com Luigi Fernandes. </Description>
+
+      <OkButton onPress={handleOkPressed}>
+
+        <OkButtonText> Ok </OkButtonText>
+
+      </OkButton>
+
+    </Container>
 
   );
 
