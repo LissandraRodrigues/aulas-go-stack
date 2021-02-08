bf3a692847b159901f484cf3c3ce3287d736e2d1
Luiza Lissandra <luizalissandrarosa@poli.ufrj.br>
2021-02-08 10:21:45 -0300

    Setting up the jest environment, creating first test from scratch and generating coverage report

---
+++ nivel-05/testes-reactjs/src/components/Tooltip/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+interface TooltipProps {
+  title: string;
+  className?: string;
+}
+
+const Tooltip: React.FC<TooltipProps> = ({
+  title,
+  children,
+  className = '',
+}) => {
+  return (
+    <Container className={className}>
+      {children}
+
+      <span> {title} </span>
+    </Container>
+  );
+};
+
+export default Tooltip;
---
+++ nivel-05/testes-reactjs/src/pages/Profile/index.tsx
@@ -0,0 +1,195 @@
+import React, { useCallback, useRef, ChangeEvent } from 'react';
+import { Form } from '@unform/web';
+import { FormHandles } from '@unform/core';
+import { FiMail, FiLock, FiUser, FiCamera, FiArrowLeft } from 'react-icons/fi';
+import * as Yup from 'yup';
+
+import { Link, useHistory } from 'react-router-dom';
+
+import api from '../../services/api';
+import { useToast } from '../../hooks/toast';
+
+import getValidationErrors from '../../utils/getValidationErrors';
+
+import { Container, Content, AvatarInput } from './styles';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+import { useAuth } from '../../hooks/auth';
+
+interface ProfileFormData {
+  name: string;
+  email: string;
+  old_password: string;
+  password: string;
+  password_confirmation: string;
+}
+
+const Profile: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
+
+  const { addToast } = useToast();
+  const history = useHistory();
+
+  const { user, updateUser } = useAuth();
+
+  const handleSubmit = useCallback(
+    async (data: ProfileFormData) => {
+      try {
+        formRef.current?.setErrors({});
+
+        const schema = Yup.object().shape({
+          name: Yup.string().required('Nome obrigatório'),
+          email: Yup.string()
+            .required('E-mail obrigatório')
+            .email('Digite um e-mail válido'),
+          old_password: Yup.string(),
+
+          password: Yup.string().when('old_password', {
+            is: true,
+            then: Yup.string().required('Campo obrigatório'),
+            otherwise: Yup.string(),
+          }),
+
+          password_confirmation: Yup.string()
+            .when('old_password', {
+              is: true,
+              then: Yup.string().required('Campo obrigatório'),
+              otherwise: Yup.string(),
+            })
+            .oneOf([Yup.ref('password'), null], 'As senhas devem ser iguais'),
+        });
+
+        await schema.validate(data, {
+          // Faz com que o Yup não pare no primeiro erro. Por padrão é true.
+          abortEarly: false,
+        });
+
+        const {
+          name,
+          email,
+          old_password,
+          password,
+          password_confirmation,
+        } = data;
+
+        const formData = {
+          name,
+          email,
+          ...(old_password
+            ? {
+                old_password,
+                password,
+                password_confirmation,
+              }
+            : {}),
+        };
+
+        const response = await api.put('/profile', formData);
+
+        updateUser(response.data);
+
+        history.push('/dashboard');
+
+        addToast({
+          type: 'success',
+          title: 'Perfil atualizado!',
+          description: 'Suas informações do perfil foram atualizadas.',
+        });
+      } catch (err) {
+        if (err instanceof Yup.ValidationError) {
+          const errors = getValidationErrors(err);
+
+          formRef.current?.setErrors(errors);
+          return;
+        }
+
+        addToast({
+          type: 'error',
+          title: 'Erro na atualização',
+          description: 'Ocorreu um erro ao atualizar perfil, tente novamente!',
+        });
+      }
+    },
+    [addToast, history, updateUser],
+  );
+
+  const handleAvatarChange = useCallback(
+    (event: ChangeEvent<HTMLInputElement>) => {
+      if (event.target.files) {
+        const data = new FormData();
+
+        data.append('avatar', event.target.files[0]);
+
+        api.patch('/users/avatar', data).then(response => {
+          updateUser(response.data);
+          addToast({
+            type: 'success',
+            title: 'Foto de perfil atualizada',
+          });
+        });
+      }
+    },
+    [addToast, updateUser],
+  );
+
+  return (
+    <Container>
+      <header>
+        <div>
+          <Link to="/dashboard">
+            <FiArrowLeft />
+          </Link>
+        </div>
+      </header>
+
+      <Content>
+        <Form
+          ref={formRef}
+          initialData={{
+            name: user.name,
+            email: user.email,
+          }}
+          onSubmit={handleSubmit}
+        >
+          <AvatarInput>
+            <img src={user.avatar_url} alt={user.name} />
+            <label htmlFor="avatar">
+              <FiCamera />
+              <input type="file" id="avatar" onChange={handleAvatarChange} />
+            </label>
+          </AvatarInput>
+
+          <h1> Meu perfil </h1>
+
+          <Input icon={FiUser} name="name" placeholder="Nome" type="text" />
+          <Input icon={FiMail} name="email" placeholder="E-mail" type="email" />
+          <Input
+            containerStyle={{ marginTop: 24 }}
+            icon={FiLock}
+            name="old_password"
+            placeholder="Senha atual"
+            type="password"
+          />
+
+          <Input
+            icon={FiLock}
+            name="password"
+            placeholder="Nova senha"
+            type="password"
+          />
+
+          <Input
+            icon={FiLock}
+            name="password_confirmation"
+            placeholder="Confirmar senha"
+            type="password"
+          />
+
+          <Button type="submit"> Confirmar mudanças </Button>
+        </Form>
+      </Content>
+    </Container>
+  );
+};
+
+export default Profile;
