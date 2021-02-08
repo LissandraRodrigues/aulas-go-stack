9c6d4e1715d50a35b6e0eb0d960a773d72fa28b6
Luiza Lissandra <luizalissandrarosa@poli.ufrj.br>
2021-02-08 10:59:39 -0300

    Login test

--- nivel-05/testes-reactjs/src/__tests__/pages/SignIn.spec.tsx
+++ nivel-05/testes-reactjs/src/__tests__/pages/SignIn.spec.tsx
@@ -1,18 +1,37 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 import SignIn from '../../pages/SignIn';
+
+const mockedHistoryPush = jest.fn();
 
 jest.mock('react-router-dom', () => {
   return {
-    useHistory: jest.fn(),
+    useHistory: () => ({
+      push: mockedHistoryPush,
+    }),
     Link: ({ children }: { children: React.ReactNode }) => children,
   };
 });
 
 describe('SignIn Page', () => {
   it('should be able to sign in', () => {
-    const { debug } = render(<SignIn />);
+    const { getByPlaceholderText, getByText } = render(<SignIn />);
 
-    debug();
+    const emailField = getByPlaceholderText('E-mail');
+    const passwordField = getByPlaceholderText('Senha');
+
+    const buttonElement = getByText('Entrar');
+
+    fireEvent.change(emailField, {
+      target: { value: 'luizadev@gmail.com' },
+    });
+
+    fireEvent.change(passwordField, {
+      target: { value: '123456' },
+    });
+
+    fireEvent.click(buttonElement);
+
+    expect(mockedHistoryPush).toHaveBeenCalledWith('/dashboard');
   });
 });
